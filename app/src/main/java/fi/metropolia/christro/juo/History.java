1c06d24390159a35c741dd8567c6cb72432354f2
Itale Tabaksmane <itale.tabaksmane@metropolia.fi>
2021-02-18 20:42:53 +0200

    second version of slider simple auto flip view

--- app/src/main/java/fi/metropolia/christro/juo/History.java
+++ app/src/main/java/fi/metropolia/christro/juo/History.java
@@ -1,25 +1,40 @@
 package fi.metropolia.christro.juo;
 
 import android.os.Bundle;
-//import android.support.wearable.activity.WearableActivity;
 import androidx.appcompat.app.AppCompatActivity;
-import android.widget.TextView;
 
-import androidx.viewpager.widget.ViewPager;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
+
 
 public class History extends AppCompatActivity {
 
-    private TextView mTextView;
-
+    ViewFlipper vf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
 
-        //mTextView = (TextView) findViewById(R.id.text);
-        ViewPager viewPager = findViewById(R.id.chart_slider);
-        ImageAdapter adapter = new ImageAdapter(this);
-        viewPager.setAdapter(adapter);
-        // Enables Always-on
+        vf = findViewById(R.id.chart_slider);
+        int[] mChartId = {R.drawable.examplegraph1,R.drawable.examplegraph2,
+                R.drawable.examplegraph3,R.drawable.examplegraph4};
+
+        for (int chart: mChartId){
+            flipperChart(chart);
+        }
+    }
+
+    public void flipperChart(int chartId){
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(chartId);
+
+        vf.addView(imageView);
+        vf.setFlipInterval(4000);
+        vf.setAutoStart(true);
+
+        vf.setInAnimation(this, android.R.anim.slide_in_left);
+        vf.setOutAnimation(this, android.R.anim.slide_out_right);
+
     }
 }
