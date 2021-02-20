0897f0a656061d5046305fba67c5aa9fca4adaaa
Christopher Romano <christopher.romano@icloud.com>
2021-02-20 14:10:31 +0200

    Revert "second version of slider simple auto flip view"
    This reverts commit 1c06d24390159a35c741dd8567c6cb72432354f2.

--- app/src/main/java/fi/metropolia/christro/juo/History.java
+++ app/src/main/java/fi/metropolia/christro/juo/History.java
@@ -1,40 +1,25 @@
 package fi.metropolia.christro.juo;
 
 import android.os.Bundle;
+//import android.support.wearable.activity.WearableActivity;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.ViewFlipper;
 
+import androidx.viewpager.widget.ViewPager;
 
 public class History extends AppCompatActivity {
 
-    ViewFlipper vf;
+    private TextView mTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
 
-        vf = findViewById(R.id.chart_slider);
-        int[] mChartId = {R.drawable.examplegraph1,R.drawable.examplegraph2,
-                R.drawable.examplegraph3,R.drawable.examplegraph4};
-
-        for (int chart: mChartId){
-            flipperChart(chart);
-        }
-    }
-
-    public void flipperChart(int chartId){
-        ImageView imageView = new ImageView(this);
-        imageView.setBackgroundResource(chartId);
-
-        vf.addView(imageView);
-        vf.setFlipInterval(4000);
-        vf.setAutoStart(true);
-
-        vf.setInAnimation(this, android.R.anim.slide_in_left);
-        vf.setOutAnimation(this, android.R.anim.slide_out_right);
-
+        //mTextView = (TextView) findViewById(R.id.text);
+        ViewPager viewPager = findViewById(R.id.chart_slider);
+        ImageAdapter adapter = new ImageAdapter(this);
+        viewPager.setAdapter(adapter);
+        // Enables Always-on
     }
 }
