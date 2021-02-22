f1ca5e6e439d66a1f87000f570042dd80a365a8d
Itale Tabaksmane <itale.tabaksmane@metropolia.fi>
2021-02-22 22:48:27 +0200

    History activity now has charts instead of images.

--- app/src/main/java/fi/metropolia/christro/juo/History.java
+++ app/src/main/java/fi/metropolia/christro/juo/History.java
@@ -3,13 +3,21 @@ package fi.metropolia.christro.juo;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.view.View;
 import android.widget.ViewFlipper;
+
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
 
 
 public class History extends AppCompatActivity {
-
+    BarChart barChart;
+    BarData barData;
+    public BarDataSet barDataSet;
+    ArrayList<BarEntry> barEntries;
     ViewFlipper vf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,11 @@ public class History extends AppCompatActivity {
         setContentView(R.layout.activity_history);
 
         vf = findViewById(R.id.chart_slider);
+
+        barDataSet = new BarDataSet(getData(),"Day Data set");
+        barChart = findViewById(R.id.barChartDay);
+        barData = new BarData(barDataSet);
+        barChart.setData(barData);
         flipperChart();
     }
 
@@ -28,4 +41,19 @@ public class History extends AppCompatActivity {
         vf.setOutAnimation(this, android.R.anim.slide_out_right);
 
     }
+
+    public ArrayList<BarEntry> getData(){
+        barEntries = new ArrayList();
+        barEntries.add(new BarEntry(1f,2));
+        barEntries.add(new BarEntry(3f,5));
+        barEntries.add(new BarEntry(10f,1));
+        barEntries.add(new BarEntry(6f,7));
+        barEntries.add(new BarEntry(4f,12));
+        return barEntries;
+    }
+
+
+
+
+
 }
