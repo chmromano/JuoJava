8c9655f93e8d62f959a59320d32b7861a456f193
Itale Tabaksmane <itale.tabaksmane@metropolia.fi>
2021-02-25 20:57:18 +0200

    New activities created: About, Help, IntakeListview and colors.xml updated

--- app/src/main/java/fi/metropolia/christro/juo/About.java
+++ app/src/main/java/fi/metropolia/christro/juo/About.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class About extends AppCompatActivity {
-
+    //text about the app
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/fi/metropolia/christro/juo/IntakeListview.java
+++ app/src/main/java/fi/metropolia/christro/juo/IntakeListview.java
@@ -3,9 +3,10 @@ package fi.metropolia.christro.juo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class IntakeListview extends AppCompatActivity {
-
+    ListView intakeList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
