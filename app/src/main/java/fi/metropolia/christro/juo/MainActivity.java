7feb907d9e3f43614eba801a643ea97d28fad109
Itale Tabaksmane <itale.tabaksmane@metropolia.fi>
2021-02-25 20:37:40 +0200

    New activities created: About, Help, IntakeListview

--- app/src/main/java/fi/metropolia/christro/juo/MainActivity.java
+++ app/src/main/java/fi/metropolia/christro/juo/MainActivity.java
@@ -1,11 +1,17 @@
 package fi.metropolia.christro.juo;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 import com.mikhaellopez.circularprogressbar.CircularProgressBar;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,11 +24,26 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
 
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //navigation
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        ////
         repository = new IntakeInputRepository(this.getApplication());
 
         textView = findViewById(R.id.intakeText);
@@ -51,5 +72,14 @@ public class MainActivity extends AppCompatActivity {
         button2.setOnClickListener(view -> {
             repository.insert(new IntakeInput(250));
         });
+    }
+    @Override
+    public void onBackPressed(){
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            //means the drawer is open
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
     }
 }
