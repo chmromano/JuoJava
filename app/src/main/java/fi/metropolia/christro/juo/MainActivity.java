71883b6451ebb9bb4ec4d6d22dc19b05a3b54bdd
Itale Tabaksmane <itale.tabaksmane@metropolia.fi>
2021-02-28 21:07:17 +0200

    Menu button works

--- app/src/main/java/fi/metropolia/christro/juo/MainActivity.java
+++ app/src/main/java/fi/metropolia/christro/juo/MainActivity.java
@@ -1,20 +1,27 @@
 package fi.metropolia.christro.juo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toolbar;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 import com.mikhaellopez.circularprogressbar.CircularProgressBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private IntakeInputViewModel intakeInputViewModel;
 
@@ -36,8 +43,13 @@ public class MainActivity extends AppCompatActivity {
         //navigation
         drawer = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
-        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ImageButton menuButton = findViewById(R.id.menu_icon);
+        menuButton.setOnClickListener(view -> {
+            drawer.openDrawer(GravityCompat.START);
+        });
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open,R.string.navigation_drawer_close);
@@ -81,5 +93,49 @@ public class MainActivity extends AppCompatActivity {
         }else {
             super.onBackPressed();
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Intent intent;
+        Context context = getApplicationContext();
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                Toast.makeText(this,"Home",Toast.LENGTH_SHORT).show();
+                intent = new Intent(context, MainActivity.class);
+            break;
+
+            case R.id.nav_mood:
+                Toast.makeText(this,"Mood",Toast.LENGTH_SHORT).show();
+                intent = new Intent(context, MoodActivity2.class);
+                break;
+
+            case R.id.nav_history:
+                Toast.makeText(this,"Charts",Toast.LENGTH_SHORT).show();
+                intent = new Intent(context, History.class);
+                break;
+
+            case R.id.nav_about:
+                Toast.makeText(this,"About",Toast.LENGTH_SHORT).show();
+                intent = new Intent(context, About.class);
+                break;
+
+            case R.id.nav_help:
+                Toast.makeText(this,"Help",Toast.LENGTH_SHORT).show();
+                intent = new Intent(context, Help.class);
+                break;
+
+            case R.id.nav_profile:
+                Toast.makeText(this,"Profile",Toast.LENGTH_SHORT).show();
+                //intent = new Intent(context,Profile.class);
+                break;
+
+            case R.id.nav_settings:
+                Toast.makeText(this,"Settings",Toast.LENGTH_SHORT).show();
+                //intent = new Intent(context, Settings.class);
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
