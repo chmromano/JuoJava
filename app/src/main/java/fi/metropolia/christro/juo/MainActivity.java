36d2ec5f686d46b7e977670927d68c6cbb6b717d
Itale Tabaksmane <itale.tabaksmane@metropolia.fi>
2021-03-02 19:07:19 +0200

    removing unnecessary functions regarding navigation

--- app/src/main/java/fi/metropolia/christro/juo/MainActivity.java
+++ app/src/main/java/fi/metropolia/christro/juo/MainActivity.java
@@ -18,6 +18,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.MenuItem.OnMenuItemClickListener;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
